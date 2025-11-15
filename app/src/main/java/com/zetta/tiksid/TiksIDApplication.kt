c26ed9f64bb39b67e6f641b36a570dbed55251bc
Jeki <ahmadzaqi490@gmail.com>
2025-11-16 00:34:19 +0700

    feat(application): add instance for coil

--- app/src/main/java/com/zetta/tiksid/TiksIDApplication.kt
+++ app/src/main/java/com/zetta/tiksid/TiksIDApplication.kt
@@ -1,16 +1,37 @@
 package com.zetta.tiksid
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 import com.zetta.tiksid.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class TiksIDApplication: Application() {
+class TiksIDApplication: Application(), ImageLoaderFactory {
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidContext(this@TiksIDApplication)
             modules(appModule)
         }
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .crossfade(300)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.40)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(this.cacheDir.resolve("image_cache"))
+                    .maxSizeBytes(200L * 1024 * 1024)
+                    .build()
+            }
+            .build()
     }
 }
